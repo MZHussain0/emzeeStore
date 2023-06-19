e5f86022d5844c3bf47511597bdd050793d3cad3
zak <mzhussain092@gmnail.com>
2023-06-19 23:44:26 +0530

    implement error handling and middleware setup

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,21 @@
 ﻿const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const ErrorHandler = require("./utils/ErrorHandler");
 const app = express();
+
+// middlewares
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(fileUpload({ useTempFiles: true }));
 
 // config
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config();
 }
 
+// errror Handling
+app.use(ErrorHandler);
 module.exports = app;
---
+++ backend/middlewares/auth.js
@@ -0,0 +1,3 @@
+﻿const ErrorHandler = require("../utils/ErrorHandler");
+const catchAsyncErrors = require("./catchAsyncError");
+const jwt = require("jsonwebtoken");
