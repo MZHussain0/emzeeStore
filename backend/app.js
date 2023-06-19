96b5447a130d7f4e774a792d4add1c94ca43c798
zak <mzhussain092@gmnail.com>
2023-06-19 23:23:24 +0530

    add mongo db connection

---
+++ backend/app.js
@@ -0,0 +1,9 @@
+﻿const express = require("express");
+const app = express();
+
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config();
+}
+
+module.exports = app;
---
+++ backend/server.js
@@ -0,0 +1,32 @@
+﻿const app = require("./app");
+const connectDatabase = require("./db/database");
+
+// handling uncaught exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to uncaught exception`);
+  process.exit(1);
+});
+
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config();
+}
+
+// connecting to database
+connectDatabase();
+
+// creating a server
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
+
+// unhandled promise rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to unhandled promise rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
